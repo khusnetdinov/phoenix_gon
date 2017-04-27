09a53f5edacfc1cd5d038f5cac67ffaeee26776e
Marat Khusnetdinov <marat@khusnetdinov.ru>
2017-04-27 18:39:55 +0300

    Tests

--- test/phoenix_gon/pipeline_test.exs
+++ test/phoenix_gon/pipeline_test.exs
@@ -2,11 +2,27 @@ defmodule PhoenixGon.PipelineTest do
   use ExUnit.Case, async: false
 
   import PhoenixGon.TestHelpers
-  import PhoenixGon.Pipeline
+
+  alias PhoenixGon.Pipeline
+  alias Plug.Conn
 
   describe "initialization" do
+    test "init" do
+      defaults = [namespace: :test]
+      expectation = %{assets: %{}, env: :test, namespace: :test}
+      actual = Pipeline.init(defaults)
+
+      assert actual == expectation
+    end
   end
 
-  describe "#call" do
+describe "connection" do
+  test "call" do
+    conn =  Pipeline.call(%Conn{}, Pipeline.init([]))
+    actual = conn.private[:phoenix_gon].env
+    expectation = Pipeline.init([]).env
+
+      assert actual == expectation
+    end
   end
 end
--- test/phoenix_gon/view_test.exs
+++ test/phoenix_gon/view_test.exs
@@ -2,8 +2,17 @@ defmodule PhoenixGon.ViewTest do
   use ExUnit.Case, async: false
 
   import PhoenixGon.TestHelpers
-  import PhoenixGon.View
+
+  alias PhoenixGon.Pipeline
+  alias Plug.Conn
 
   describe "#render_gon_script" do
+    test 'text' do
+      conn = Pipeline.call(%Conn{}, Pipeline.init([]))
+
+      actual = PhoenixGon.View.render_gon_script(conn)
+
+      assert {:safe, _} = actual
+    end
   end
 end
