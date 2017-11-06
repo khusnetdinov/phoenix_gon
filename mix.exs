8380e02fe8ee8d3c7ad059579114d11248605177
Joseph An <an.joseph891@gmail.com>
2017-11-06 00:05:11 -0500

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixGon.Mixfile do
 
   def project do
     [app: :phoenix_gon,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
