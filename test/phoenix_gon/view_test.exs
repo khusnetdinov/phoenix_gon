80b4c90c90cfbf26d597649b641b59508b9b0064
Fernando <fernando@jabberfest.com>
2018-01-17 22:15:24 -0500

    Sanitize output generated by call to Poison.encode
    * Created escape_assets function in PhoenixGon.View
    * Refactored script method in PhoenixGon.View to call escape_assets
    * Added PhoenixGon.View test to test for correctly escaped javascript
    * Bumped package version from 0.2.1 to 0.2.2

--- test/phoenix_gon/view_test.exs
+++ test/phoenix_gon/view_test.exs
@@ -2,6 +2,7 @@ defmodule PhoenixGon.ViewTest do
   use ExUnit.Case, async: false
 
   import PhoenixGon.TestHelpers
+  import PhoenixGon.Controller
 
   alias PhoenixGon.Pipeline
   alias Plug.Conn
@@ -9,10 +10,22 @@ defmodule PhoenixGon.ViewTest do
   describe "#render_gon_script" do
     test 'text' do
       conn = Pipeline.call(%Conn{}, Pipeline.init([]))
-
+      
       actual = PhoenixGon.View.render_gon_script(conn)
 
       assert {:safe, _} = actual
+    end
+  end
+
+  describe "#escape_assets escapes javascript" do
+    test 'text' do
+      conn = Pipeline.call(%Conn{}, Pipeline.init([]))
+      conn = put_gon(conn, :malicious, "all your bases</script><script>alert('are belong to us!')</script>")
+      
+      actual = PhoenixGon.View.escape_assets(conn)
+      expected = "{\\\"malicious\\\":\\\"all your bases<\\/script><script>alert(\\'are belong to us!\\')<\\/script>\\\"}"
+
+      assert expected == actual
     end
   end
 end
