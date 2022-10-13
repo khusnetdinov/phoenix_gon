370fd06c6ef3777216888fae350c088f3392e45a
Титова Татьяна <titovatatana@MacBook-Pro-Titova.local>
2022-10-13 19:11:06 +0300

    Elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixGon.Mixfile do
     [
       app: :phoenix_gon,
       version: "0.4.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
